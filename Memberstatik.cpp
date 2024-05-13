627e93ee5b6d58646eba2a247ab42bdd2d3b56d7
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-13 10:29:05 +0700

    Memasukan member

--- Memberstatik.cpp
+++ Memberstatik.cpp
@@ -7,4 +7,12 @@ class mahasiswa
      static int nim;
      int id;
      string nama;
+
+     void setID();
+     void printAll();
+     mahasiswa(string pnama)
+     {
+        nama = pnama;
+        setID();
+     }
 };
