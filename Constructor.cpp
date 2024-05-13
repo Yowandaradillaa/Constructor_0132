89bc521ff55d36134e9e37ceb567c07a5bb2784d
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-13 11:17:51 +0700

    Memasukan class mahasiswa

--- Constructor.cpp
+++ Constructor.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class mahasiswa {
+    public : 
+    int nim;
+    string nama;
+
+    public:
+    mahasiswa()
+    {
+        nim = 0;
+        nama = " ";
+    }
+};
+
