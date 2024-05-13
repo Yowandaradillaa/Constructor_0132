414466e27857706a6e9efdd85c73aeaa45474570
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-13 10:54:09 +0700

    Memasukan static void dan int

--- Memberstatik2.cpp
+++ Memberstatik2.cpp
@@ -11,5 +11,8 @@ class mahasiswa {
     void setID();
     void printAll();
 
-    
+    static void setNim(int pNim) {nim = pNim;}
+    static int getNim() {return nim;}
+
+    mahasiswa (string pnama) : nama (pnama) {setID();}
 };
